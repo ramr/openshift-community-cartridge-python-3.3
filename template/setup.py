c285a0ea43fba4483a752c53a9baf93fe4a52503
ramr <smitram@gmail.com>
2013-02-01 16:52:25 -0800

    Add distro + version + arch specific package support + add a stub/sample fedora/$release/$arch directory.

--- template/setup.py
+++ template/setup.py
@@ -7,7 +7,7 @@ setup(name='YourAppName', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['WebOb'
+      install_requires=['WebOb',
                         #  'pymongo3',
                         #  'psycopg2',
       ],
